0abb316bab8a6bdd0ed2fe1de963832585ff095e
jazmin114 <anesquivelma@est.utn.ac.cr>
2025-08-02 02:14:45 -0600

    Galeria, FuncionCargarGaleria

--- JS/imagenes.js
+++ JS/imagenes.js
@@ -15,6 +15,32 @@ fetch("JSON/imagenes.json")
       });
     }
   });
+  function cargarGaleria(rutaJson, idGaleria) {
+  fetch(rutaJson)
+    .then((response) => response.json())
+    .then((data) => {
+      const galeria = document.getElementById(idGaleria);
+      if (galeria) {
+        data.forEach((prenda) => {
+          const col = document.createElement("div");
+          col.className = "col-md-4 col-sm-6";
+
+          col.innerHTML = `
+            <div class="card shadow-sm">
+              <img src="${prenda.url}" class="card-img-top" alt="${prenda.alt}">
+              <div class="card-body text-center">
+                <h5 class="card-title">${prenda.nombre}</h5>
+              </div>
+            </div>
+          `;
+
+          galeria.appendChild(col);
+        });
+      }
+    })
+    .catch((error) => console.error("Error al cargar la galería:", error));
+}
+
 
 
 
