457193fcc005ebb1009c19f1e5caf3a0b948084c
jazmin114 <anesquivelma@est.utn.ac.cr>
2025-08-19 10:09:34 -0600

    Ruta-Ubi

--- JS/mapa.js
+++ JS/mapa.js
@@ -1,43 +1,65 @@
-
 function initMap() {
-  const lugarFijo = { lat: 10.020843, lng: -84.223928  }; 
+  const lugarFijo = { lat: 10.020843, lng: -84.223928 };
 
-  // Mapa centrado inicialmente en el lugar fijo
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 16,
-    center: lugarFijo,
+    center: lugarFijo
   });
 
-  // Marcador para el lugar fijo
+  // Marcador del lugar fijo
   const marcadorLugarFijo = new google.maps.Marker({
     position: lugarFijo,
     map: map,
     title: "Pueblo Nuevo, Alajuela",
-    icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png", // marcador rojo para el lugar fijo
+    icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
   });
 
-  // Intentar obtener ubicación actual del usuario
+  // Servicios para rutas
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer({
+    map: map,
+    suppressMarkers: true,
+    polylineOptions: {
+      strokeColor: "#FF6FB5",
+      strokeWeight: 5
+    }
+  });
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const ubicacionUsuario = {
           lat: position.coords.latitude,
-          lng: position.coords.longitude,
+          lng: position.coords.longitude
         };
 
-        // Marcador para la ubicación del usuario
+        // Marcador azul del usuario
         const marcadorUsuario = new google.maps.Marker({
           position: ubicacionUsuario,
           map: map,
           title: "Tu ubicación",
-          icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png", // marcador azul para el usuario
+          icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
         });
 
-        // Ajustar el mapa para que muestre ambos puntos
-        const bounds = new google.maps.LatLngBounds();
-        bounds.extend(lugarFijo);
-        bounds.extend(ubicacionUsuario);
-        map.fitBounds(bounds);
+        // 🚗 Solicitar ruta y dibujarla
+        directionsService.route(
+          {
+            origin: ubicacionUsuario,
+            destination: lugarFijo,
+            travelMode: google.maps.TravelMode.DRIVING
+          },
+          (result, status) => {
+            if (status === "OK") {
+              directionsRenderer.setDirections(result);
+            } else {
+              console.warn("No se pudo trazar la ruta:", status);
+              const bounds = new google.maps.LatLngBounds();
+              bounds.extend(lugarFijo);
+              bounds.extend(ubicacionUsuario);
+              map.fitBounds(bounds);
+            }
+          }
+        );
       },
       () => {
         alert("No se pudo obtener tu ubicación.");
@@ -47,5 +69,4 @@ function initMap() {
     alert("Tu navegador no soporta geolocalización.");
   }
 }
-
 
